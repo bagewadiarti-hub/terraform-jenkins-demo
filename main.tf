bcaa2aa4cb40f2584f5343b70fa65b97f0f96c51
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-26 14:46:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+   docker = {
+    source = "kreuzwerker/docker"
+    version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resouce "docker_image" "nginx" {
+  name = "nginx:alpine"
+  keep_locally = true
+}
+
+resource "docker_container" "web" {
+  name = "jenkins-terraform-ngnix"
+  image = docker_image.ngnix.image_id
+
+ports {
+  internal = 80
+  external = 8090
+}
+labels {
+  label = "deployed_by"
+  value = "jenkins-terraform"
+}
+}
+
+output "container_name" {
+  value = docker_container.web.name
+}
+
+output "container_url" {
+  value = ""http://localhost:8090"
+}
+
+
